aa24d5a97238e52ea8044fec0fa1438d585cc323
Calle Englund <calle.englund@saabgroup.com>
2016-09-07 15:00:34 +0200

    rdoc for KVDAG::AttributeNode

--- lib/kvdag/attrnode.rb
+++ lib/kvdag/attrnode.rb
@@ -1,6 +1,18 @@
 class KVDAG
+
+  # Mixin with common methods for managing the +attrs+ of
+  # vertices and edges in a KVDAG.
+
   module AttributeNode
     attr_reader :attrs
+
+    # Returns the value for an +attr+. If the +attr+ can't be found,
+    # it will raise a KeyError exception.
+    #
+    # +options+:
+    #   shallow: If true, lookup is limited to attrs defined in
+    #            this attrnode. The default is lookup in the tree
+    #            returned by +to_hash_proxy+.
 
     def fetch(attr, options = {})
       case
@@ -13,11 +25,20 @@ class KVDAG
       end
     end
 
+    # Return the value for an +attr+, or +nil+ if the +attr+ can't be found.
+    #
+    # +options+:
+    #   shallow: If true, lookup is limited to attrs defined in
+    #            this attrnode. The default is lookup in the tree
+    #            returned by +to_hash_proxy+.
+
     def [](attr, options = {})
       fetch(attr, options)
     rescue
       nil
     end
+
+    # Set the +value+ for an +attr+ in this attrnode.
 
     def []=(attr, value)
       @attrs[attr] = value
