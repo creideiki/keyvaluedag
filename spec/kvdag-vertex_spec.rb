19e0d5dd34ad6ab5e65f3bc0bad74c7265436e4d
Karl-Johan Karlsson <karl-johan.karlsson@saabgroup.com>
2016-10-26 16:32:40 +0200

    Add test cases for #parents and #children

--- spec/kvdag-vertex_spec.rb
+++ spec/kvdag-vertex_spec.rb
@@ -1,6 +1,39 @@
 require 'spec_helper'
 
 describe KVDAG::Vertex do
+  context 'immediate ancestry' do
+    before :all do
+      # v1 --> v2 --> v3
+      #           \-> v4
+
+      @dag = KVDAG.new
+      @v1 = @dag.vertex
+
+      @v2 = @dag.vertex
+      @v1.edge @v2
+
+      @v3 = @dag.vertex
+      @v4 = @dag.vertex
+      @v2.edge @v3
+      @v2.edge @v4
+    end
+
+    it 'finds parents' do
+      parents = @v2.parents
+      expect(parents).not_to include @v1
+      expect(parents).not_to include @v2
+      expect(parents).to include @v3
+      expect(parents).to include @v4
+    end
+
+    it 'finds children' do
+      children = @v2.children
+      expect(children).to include @v1
+      expect(children).not_to include @v2
+      expect(children).not_to include @v3
+      expect(children).not_to include @v4
+    end
+  end
 
   context 'deep ancestry' do
     before :all do
